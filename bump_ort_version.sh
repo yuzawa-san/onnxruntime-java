5864c476a7ea6806b64d30aee8ea173c14f69c64
James Yuzawa <jtyuzawa@gmail.com>
2022-05-12 19:56:04 -0400

    Create bump_ort_version.sh

---
+++ bump_ort_version.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+NEW_VERSION=$(curl -s https://api.github.com/repos/microsoft/onnxruntime/tags | jq -r ".[0].name" | sed 's/^v//g')
+read -p "Use version $NEW_VERSION (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        echo "Using version $NEW_VERSION"
+    ;;
+    * )
+        exit 1;
+    ;;
+esac
+git checkout master 
+git pull
+sed -i '' "s/^com.jyuzawa.onnxruntime.library_version=.*/com.jyuzawa.onnxruntime.library_version=$NEW_VERSION/" gradle.properties
+./gradlew clean jextractClean jextract 
+./gradlew build
+git checkout -b ort/$NEW_VERSION
+git commit -am "ORT v$NEW_VERSION"
