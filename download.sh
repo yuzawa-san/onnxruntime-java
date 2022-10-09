44b488473c650ed513e4cd2d7a85274621469de1
James Yuzawa <jtyuzawa@gmail.com>
2022-10-09 19:26:40 -0400

    migrate to jextract and away from makefile

--- download.sh
+++ download.sh
@@ -1,35 +1,61 @@
 #!/bin/sh
 
-ORT_VERSION=$1
-OS_ARCH=$2
-GPU_SUFFIX=$3
-DOWNLOAD_NAME=$4
-COMPRESSION=$5
-LIBRARY_SUFFIX=$6
+doIt () {
+  OS_ARCH=$1
+  GPU_SUFFIX=$2
+  DOWNLOAD_NAME=$3
+  COMPRESSION=$4
+  LIBRARY_SUFFIX=$5
+  mkdir -p build/onnxruntime-${ORT_VERSION}
+  FILENAME=onnxruntime-${DOWNLOAD_NAME}-${ORT_VERSION}.${COMPRESSION}
+  cd cache/
+  if [ -f "${FILENAME}" ]; then
+    echo "HIT ${FILENAME}"
+  else
+    echo "MISS ${FILENAME}"
+    curl -OL https://github.com/microsoft/onnxruntime/releases/download/v${ORT_VERSION}/${FILENAME}
+  fi
+  cd ../build/onnxruntime-${ORT_VERSION}
+  ln -s ../../cache/${FILENAME} ${FILENAME}
+  if [ "${COMPRESSION}" = "zip" ]; then
+    unzip ${FILENAME}
+  else
+    # https://github.com/msys2/MSYS2-packages/issues/1216
+    # https://github.com/heroku/heroku-slugs/issues/3
+    tar xvzf ${FILENAME} || tar xvzf ${FILENAME}
+  fi
 
+  mkdir -p ${OS_ARCH}${GPU_SUFFIX}/com/jyuzawa/onnxruntime/native/${OS_ARCH}
+  for i in $(ls onnxruntime-${DOWNLOAD_NAME}-${ORT_VERSION}/lib | grep "\.${LIBRARY_SUFFIX}" | grep -v "${ORT_VERSION}"); do
+    cp onnxruntime-${DOWNLOAD_NAME}-${ORT_VERSION}/lib/$i ${OS_ARCH}${GPU_SUFFIX}/com/jyuzawa/onnxruntime/native/${OS_ARCH}
+  done
+  cd ${OS_ARCH}${GPU_SUFFIX}/com/jyuzawa/onnxruntime/native/${OS_ARCH}
+  ls | grep -v libraries > libraries
+}
 
-mkdir -p build/onnxruntime-${ORT_VERSION}
-FILENAME=onnxruntime-${DOWNLOAD_NAME}-${ORT_VERSION}.${COMPRESSION}
-cd cache/
-if [ -f "${FILENAME}" ]; then
-  echo "HIT ${FILENAME}"
-else
-  echo "MISS ${FILENAME}"
-  curl -OL https://github.com/microsoft/onnxruntime/releases/download/v${ORT_VERSION}/${FILENAME}
-fi
-cd ../build/onnxruntime-${ORT_VERSION}
-ln -s ../../cache/${FILENAME} ${FILENAME}
-if [ "${COMPRESSION}" = "zip" ]; then
-  unzip ${FILENAME}
-else
-  # https://github.com/msys2/MSYS2-packages/issues/1216
-  # https://github.com/heroku/heroku-slugs/issues/3
-  tar xvzf ${FILENAME} || tar xvzf ${FILENAME}
-fi
-
-mkdir -p ${OS_ARCH}${GPU_SUFFIX}/com/jyuzawa/onnxruntime/native/${OS_ARCH}
-for i in $(ls onnxruntime-${DOWNLOAD_NAME}-${ORT_VERSION}/lib | grep "\.${LIBRARY_SUFFIX}" | grep -v "${ORT_VERSION}"); do
-  cp onnxruntime-${DOWNLOAD_NAME}-${ORT_VERSION}/lib/$i ${OS_ARCH}${GPU_SUFFIX}/com/jyuzawa/onnxruntime/native/${OS_ARCH}
-done
-cd ${OS_ARCH}${GPU_SUFFIX}/com/jyuzawa/onnxruntime/native/${OS_ARCH}
-ls | grep -v libraries > libraries
+case $VARIANT in
+  linux-aarch_64)
+    doIt linux-aarch_64 "" linux-aarch64 tgz so
+    ;;
+  linux-x86_64)
+    doIt linux-x86_64 "" linux-x64 tgz so
+    ;;
+  linux-x86_64-gpu)
+    doIt linux-x86_64 -gpu linux-x64-gpu tgz so
+    ;;
+  osx-x86_64)
+    doIt osx-x86_64 "" osx-x86_64 tgz dylib
+    ;;
+  osx-aarch_64)
+    doIt osx-aarch_64 "" osx-arm64 tgz dylib
+    ;;
+  windows-aarch_64)
+    doIt windows-aarch_64 "" win-arm64 zip dll
+    ;;
+  windows-x86_64)
+    doIt windows-x86_64 "" win-x64 zip dll
+    ;;
+  windows-x86_64-gpu)
+    doIt windows-x86_64 -gpu win-x64-gpu zip dll
+    ;;
+esac
--- jextract.sh
+++ jextract.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-sed -i .bak 's/size_t/unsigned long long/g' build/onnxruntime-${ORT_VERSION}/onnxruntime-linux-x64-gpu-${ORT_VERSION}/include/onnxruntime_c_api.h
-sed -i .bak 's/uint64_t/unsigned long long/g' build/onnxruntime-${ORT_VERSION}/onnxruntime-linux-x64-gpu-${ORT_VERSION}/include/onnxruntime_c_api.h
-sed -i .bak 's/int64_t/long long/g' build/onnxruntime-${ORT_VERSION}/onnxruntime-linux-x64-gpu-${ORT_VERSION}/include/onnxruntime_c_api.h
-rm build/onnxruntime-${ORT_VERSION}/onnxruntime-linux-x64-gpu-${ORT_VERSION}/include/onnxruntime_c_api.h.bak
-docker run --rm -v $(PWD):/workdir `docker build -q .` --output /workdir/src/main/java -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I build/onnxruntime-${ORT_VERSION}/onnxruntime-linux-x64-gpu-${ORT_VERSION}/include --dump-includes symbols.conf onnxruntime_all.h
+GENERATED_DIR=build/generated/source/jextract
+OS_ARCH=linux-x64-gpu
+HEADER_DIR=build/onnxruntime-${ORT_VERSION}/onnxruntime-${OS_ARCH}-${ORT_VERSION}/include
+HEADER_FILE=${HEADER_DIR}/onnxruntime_c_api.h
+sed -i .bak 's/size_t/unsigned long long/g' ${HEADER_FILE}
+sed -i .bak 's/uint64_t/unsigned long long/g' ${HEADER_FILE}
+sed -i .bak 's/int64_t/long long/g' ${HEADER_FILE}
+rm ${HEADER_FILE}.bak
+docker run --rm -v $(PWD):/workdir `docker build -q .` --output ${GENERATED_DIR} -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I ${HEADER_DIR} --dump-includes symbols.conf onnxruntime_all.h
 # strip out the irrelevant symbols
 csplit symbols.conf "/onnxruntime_c_api.h/"
 rm xx00
 mv xx01 symbols.conf
-docker run --rm -v $(PWD):/workdir `docker build -q .` --output /workdir/src/main/java -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I build/onnxruntime-${ORT_VERSION}/onnxruntime-linux-x64-gpu-${ORT_VERSION}/include @symbols.conf onnxruntime_all.h
+docker run --rm -v $(PWD):/workdir `docker build -q .` --output ${GENERATED_DIR} -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I ${HEADER_DIR} @symbols.conf onnxruntime_all.h
 # strip out loads since we'll manage load
-sed -i .bak '/System.loadLibrary/d' src/main/java/com/jyuzawa/onnxruntime_extern/RuntimeHelper.java
-rm src/main/java/com/jyuzawa/onnxruntime_extern/RuntimeHelper.java.bak
+RUNTIME_HELPER=${GENERATED_DIR}/com/jyuzawa/onnxruntime_extern/RuntimeHelper.java
+sed -i .bak '/System.loadLibrary/d' ${RUNTIME_HELPER}
+rm ${RUNTIME_HELPER}.bak
