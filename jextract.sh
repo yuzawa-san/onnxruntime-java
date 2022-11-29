074f9c40bc99e85b50140329558296c4fcd4e6de
James Yuzawa <jtyuzawa@gmail.com>
2022-11-29 17:01:04 -0500

    try to fix perms

--- jextract.sh
+++ jextract.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 GENERATED_DIR=build/generated/source/jextract
+PACKAGE_DIR=${GENERATED_DIR}/com/jyuzawa/onnxruntime_extern
 SYMBOLS_CONF=${GENERATED_DIR}/symbols.conf
 HEADER_DIR=build/onnxruntime/linux-x86_64-gpu/include
 HEADER_FILE=${HEADER_DIR}/onnxruntime_c_api.h
+mkdir -p ${PACKAGE_DIR}
 docker run --rm -v `pwd`:/workdir `docker build -q .` --output ${GENERATED_DIR} -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I ${HEADER_DIR} --dump-includes ${SYMBOLS_CONF} onnxruntime_all.h
 # strip out the irrelevant symbols
 csplit ${SYMBOLS_CONF} "/onnxruntime_c_api.h/"
@@ -11,6 +13,6 @@ rm xx00
 mv xx01 ${SYMBOLS_CONF}
 docker run --rm -v `pwd`:/workdir `docker build -q .` --output ${GENERATED_DIR} -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I ${HEADER_DIR} @${SYMBOLS_CONF} onnxruntime_all.h
 # strip out loads since we'll manage load
-RUNTIME_HELPER=${GENERATED_DIR}/com/jyuzawa/onnxruntime_extern/RuntimeHelper.java
+RUNTIME_HELPER=${PACKAGE_DIR}/RuntimeHelper.java
 grep -v "System.loadLibrary" < ${RUNTIME_HELPER} > ${RUNTIME_HELPER}.bak
 mv ${RUNTIME_HELPER}.bak ${RUNTIME_HELPER}
