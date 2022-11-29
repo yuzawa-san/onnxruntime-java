6fad8b710eff2892119d4d6850928f296e442800
James Yuzawa <jtyuzawa@gmail.com>
2022-11-29 17:58:28 -0500

    move jextracts

--- jextract.sh
+++ jextract.sh
@@ -6,12 +6,12 @@ SYMBOLS_CONF=${GENERATED_DIR}/symbols.conf
 HEADER_DIR=build/onnxruntime/linux-x86_64-gpu/include
 HEADER_FILE=${HEADER_DIR}/onnxruntime_c_api.h
 mkdir -p ${PACKAGE_DIR}
-./jextract-19/bin/jextract --output ${GENERATED_DIR} -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I ${HEADER_DIR} --dump-includes ${SYMBOLS_CONF} onnxruntime_all.h
+jextract --output ${GENERATED_DIR} -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I ${HEADER_DIR} --dump-includes ${SYMBOLS_CONF} onnxruntime_all.h
 # strip out the irrelevant symbols
 csplit ${SYMBOLS_CONF} "/onnxruntime_c_api.h/"
 rm xx00
 mv xx01 ${SYMBOLS_CONF}
-./jextract-19/bin/jextract --output ${GENERATED_DIR} -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I ${HEADER_DIR} @${SYMBOLS_CONF} onnxruntime_all.h
+jextract --output ${GENERATED_DIR} -l onnxruntime --source --target-package com.jyuzawa.onnxruntime_extern -I /usr/include -I ${HEADER_DIR} @${SYMBOLS_CONF} onnxruntime_all.h
 # strip out loads since we'll manage load
 RUNTIME_HELPER=${PACKAGE_DIR}/RuntimeHelper.java
 grep -v "System.loadLibrary" < ${RUNTIME_HELPER} > ${RUNTIME_HELPER}.bak
