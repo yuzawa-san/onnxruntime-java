2a4e937d59b138746b377aacfaf3ef94dbd1eb3f
James Yuzawa <jtyuzawa@gmail.com>
2023-04-30 13:48:32 -0400

    move benchmark

--- onnxruntime-benchmark/benchmark.sh
+++ onnxruntime-benchmark/benchmark.sh
@@ -3,7 +3,7 @@
 mkdir -p build/jfr
 JFR=build/jfr/ort-`date +"%Y-%m-%dT%H-%M-%S%z"`.jfr
 echo "writing output to ${JFR}";
-export JAVA_HOME=/Users/jtyuzawa/Documents/personal_repos/jdk/build/macosx-x86_64-server-release/jdk
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-20.jdk/Contents/Home
 java -version
 export ONNXRUNTIME_BENCHMARK_OPTS="-agentpath:/Users/jtyuzawa/Downloads/async-profiler-2.9-macos/build/libasyncProfiler.dylib=start,event=cpu,alloc,lock,file=${JFR}"
 exec build/install/onnxruntime-benchmark/bin/onnxruntime-benchmark
