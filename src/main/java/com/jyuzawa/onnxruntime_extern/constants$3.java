b706ba302cf74a5101d20508b15c9be688ae6537
James Yuzawa <jtyuzawa@gmail.com>
2023-09-20 20:09:18 -0400

    ORT v1.16.0 jextract

---
+++ src/main/java/com/jyuzawa/onnxruntime_extern/constants$3.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 James Yuzawa (https://www.jyuzawa.com/)
+ * SPDX-License-Identifier: MIT
+ */
+package com.jyuzawa.onnxruntime_extern;
+
+import static java.lang.foreign.ValueLayout.*;
+
+import java.lang.foreign.*;
+
+class constants$3 {
+
+    static final MemorySegment ORT_FILE$SEGMENT =
+            RuntimeHelper.CONSTANT_ALLOCATOR.allocateUtf8String("/tmp/jextract$14896501309904306054.h");
+}
