30f29c074225e5665b68592bf0fd81b5854767d3
James Yuzawa <jtyuzawa@gmail.com>
2024-02-15 20:32:47 -0500

    upgrade openvino

--- src/main/java/com/jyuzawa/onnxruntime/ExecutionProviderOpenVINOConfig.java
+++ src/main/java/com/jyuzawa/onnxruntime/ExecutionProviderOpenVINOConfig.java
@@ -4,29 +4,25 @@
  */
 package com.jyuzawa.onnxruntime;
 
-import com.jyuzawa.onnxruntime_extern.OrtOpenVINOProviderOptions;
 import java.lang.foreign.MemoryAddress;
-import java.lang.foreign.MemorySegment;
 import java.lang.foreign.MemorySession;
 import java.util.Map;
 
-final class ExecutionProviderOpenVINOConfig extends ExecutionProviderConfig {
+final class ExecutionProviderOpenVINOConfig extends ExecutionProviderMapConfig {
 
     protected ExecutionProviderOpenVINOConfig(Map<String, String> properties) {
         super(properties);
     }
 
     @Override
-    final void appendToSessionOptions(MemorySession memorySession, ApiImpl api, MemoryAddress sessionOptions) {
-        MemorySegment config = OrtOpenVINOProviderOptions.allocate(memorySession);
-        copyString("device_type", config, memorySession, OrtOpenVINOProviderOptions::device_type$set);
-        copyByte("enable_vpu_fast_compile", config, OrtOpenVINOProviderOptions::enable_vpu_fast_compile$set);
-        copyString("device_id", config, memorySession, OrtOpenVINOProviderOptions::device_id$set);
-        copyLong("num_of_threads", config, OrtOpenVINOProviderOptions::num_of_threads$set);
-        copyString("cache_dir", config, memorySession, OrtOpenVINOProviderOptions::cache_dir$set);
-        // TODO: context
-        copyByte("enable_opencl_throttling", config, OrtOpenVINOProviderOptions::enable_opencl_throttling$set);
-        copyByte("enable_dynamic_shapes", config, OrtOpenVINOProviderOptions::enable_dynamic_shapes$set);
-        api.checkStatus(api.SessionOptionsAppendExecutionProvider_OpenVINO.apply(sessionOptions, config.address()));
+    protected void appendToSessionOptions(
+            MemorySession memorySession,
+            ApiImpl api,
+            MemoryAddress sessionOptions,
+            MemoryAddress keys,
+            MemoryAddress values,
+            int numProperties) {
+        api.checkStatus(api.SessionOptionsAppendExecutionProvider_OpenVINO_V2.apply(
+                sessionOptions, keys, values, numProperties));
     }
 }
