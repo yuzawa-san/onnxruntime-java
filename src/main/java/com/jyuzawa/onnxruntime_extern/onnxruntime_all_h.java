706e5ed69dc349c06e9b2049c1b53b9ed5f53be8
James Yuzawa <jtyuzawa@gmail.com>
2024-11-25 16:30:12 -0500

    ORT v1.20.1 jextract

--- src/main/java/com/jyuzawa/onnxruntime_extern/onnxruntime_all_h.java
+++ src/main/java/com/jyuzawa/onnxruntime_extern/onnxruntime_all_h.java
@@ -1485,13 +1485,13 @@ public class onnxruntime_all_h {
     }
     /**
      * {@snippet lang=c :
-     * #define ORT_FILE "/var/folders/_0/vb3rmc0x05xfzm34qqcsmqk40000gn/T/jextract$8967177121571697683.h"
+     * #define ORT_FILE "/var/folders/_0/vb3rmc0x05xfzm34qqcsmqk40000gn/T/jextract$120974830120199492.h"
      * }
      */
     public static MemorySegment ORT_FILE() {
         class Holder {
             static final MemorySegment ORT_FILE = onnxruntime_all_h.LIBRARY_ARENA.allocateFrom(
-                    "/var/folders/_0/vb3rmc0x05xfzm34qqcsmqk40000gn/T/jextract$8967177121571697683.h");
+                    "/var/folders/_0/vb3rmc0x05xfzm34qqcsmqk40000gn/T/jextract$120974830120199492.h");
         }
         return Holder.ORT_FILE;
     }
