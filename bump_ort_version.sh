d7c6deba6198468535ccfc5c1dc0444a1713365f
James Yuzawa <jtyuzawa@gmail.com>
2026-02-23 20:21:14 -0500

    upgrade script

--- bump_ort_version.sh
+++ bump_ort_version.sh
@@ -1,27 +1,20 @@
 #!/bin/sh
 
-docker ps > /dev/null 2>&1
-[ $? -ne 0 ] && echo "please start docker desktop" && exit 1
-
+OLD_VERSION=$(cat ORT_VERSION)
 NEW_VERSION=$(curl -s https://api.github.com/repos/microsoft/onnxruntime/tags | jq -r ".[0].name" | sed 's/^v//g')
-read -p "Use version $NEW_VERSION (y/n)? " answer
-case ${answer:0:1} in
-    y|Y )
-        echo "Using version $NEW_VERSION"
-    ;;
-    * )
-        exit 1;
-    ;;
-esac
-git checkout ort/$NEW_VERSION
-[ $? -ne 0 ] && echo "please delete existing branch ort/$NEW_VERSION" && exit 1
+echo "old: $OLD_VERSION"
+echo "new: $NEW_VERSION"
+if [ "$OLD_VERSION" = "$NEW_VERSION" ]; then
+  echo "::notice::up to date"
+  exit 0
+fi
+BRANCH=orttesting/$NEW_VERSION
+git checkout -b $BRANCH
+if [ $? -ne 0 ]; then
+   echo "::notice::branch $BRANCH already exists"
+   exit 0
+fi
 
-git checkout master && \
-git pull && \
-git checkout -b ort/$NEW_VERSION && \
-sed -i '' "s/^com.jyuzawa.onnxruntime.library_version=.*/com.jyuzawa.onnxruntime.library_version=$NEW_VERSION/" gradle.properties && \
-git commit -am "ORT v$NEW_VERSION bump" && \
-./gradlew jextract && \
-git add src/main/java/com/jyuzawa/onnxruntime_extern && \
-git commit -am "ORT v$NEW_VERSION jextract"
-./gradlew clean build
+echo $NEW_VERSION > ORT_VERSION
+git commit -am "ORT v$NEW_VERSION bump"
+#git push origin $BRANCH
