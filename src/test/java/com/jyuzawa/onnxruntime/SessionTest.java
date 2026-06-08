feb6a4839830973e6a5f5975ba231477459b7e14
James Yuzawa <jtyuzawa@gmail.com>
2026-06-08 08:19:23 -0400

    Update SessionTest.java

--- src/test/java/com/jyuzawa/onnxruntime/SessionTest.java
+++ src/test/java/com/jyuzawa/onnxruntime/SessionTest.java
@@ -87,7 +87,7 @@ public class SessionTest {
 
     private ByteBuffer identityModel(TypeProto type) {
         return ModelProto.newBuilder()
-                .setIrVersion(25)
+                .setIrVersion(13)
                 .addOpsetImport(OperatorSetIdProto.newBuilder().setVersion(15))
                 .setGraph(GraphProto.newBuilder()
                         .addNode(NodeProto.newBuilder()
